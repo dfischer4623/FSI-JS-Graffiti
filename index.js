8edd086cf42a02a1151461448310a26143ac56b1
David Fischer <dfischer@fischerltd.com>
2022-06-09 19:03:50 -0400

    commit

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 
 // Select some elements...
 
-let header = document.querySelector('#page-header')
+let header = document.querySelector('#page-header')         // id selector
 header.style.textAlign = "left";
 
-let dogImages = document.querySelectorAll('.dog-image')
+let dogImages = document.querySelectorAll('.dog-image')     // class selector
 for(let i = 0; i < dogImages.length; i++){
 	dogImages[i].style.borderRadius = '50px';
     dogImages[i].style.transform = "rotate(180deg)";
 }
 
-let dog = document.querySelectorAll('.dog-name')
+let dog = document.querySelectorAll('.dog-name')            // class selector
 for (let i=0; i<dog.length; i++) {
     dog[i].style.textAlign = "left";
 }
